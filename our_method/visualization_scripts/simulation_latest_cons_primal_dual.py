b54567532b62b86724b84f6514b3107cc96b5ae8
ghimiremukesh <ghimiremukesh1@gmail.com>
2024-09-30 21:04:49 -0700

    updates

--- our_method/visualization_scripts/simulation_latest_cons_primal_dual.py
+++ our_method/visualization_scripts/simulation_latest_cons_primal_dual.py
@@ -58,7 +58,7 @@ matplotlib.use('TkAgg')
 # jax.config.update("jax_disable_jit", True)
 
 EPSILON = 1e-6
-p_hat_bound = 1.4
+p_hat_bound = 11
 # for GDA steps
 primal_bounds = jnp.concatenate((
     jnp.array([[-6., 6.]]),
@@ -79,16 +79,6 @@ dual_bounds = jnp.concatenate((
     jnp.array([[-6., 6.]]),
     jnp.array([[-4., 4.]]),
     jnp.array([[-6., 6.]]),
-    jnp.array([[-4., 4.]]),
-    jnp.array([[-6., 6.]]),
-    jnp.array([[-4., 4.]]),
-    jnp.array([[EPSILON, 1 - EPSILON]] * 2),
-    jnp.array([[-p_hat_bound, p_hat_bound]] * 6),
-    jnp.array([[-6., 6.]]),
-    jnp.array([[-12., 12.]]),
-    jnp.array([[-6., 6.]]),
-    jnp.array([[-12., 12.]]),
-    jnp.array([[-6., 6.]]),
     jnp.array([[-12., 12.]]),
 ))
 
@@ -96,7 +86,7 @@ min_bounds_dual = dual_bounds[:, 0]
 max_bounds_dual = dual_bounds[:, 1]
 
 
-chkpnts = [f'../logs_cons_dual/t_{i}/checkpoint_0/checkpoint' for i in range(1, 11)]
+chkpnts = [f'../logs_cons_dual_no_split/t_{i}/checkpoint_0/checkpoint' for i in range(1, 11)]
 
 
 
@@ -104,13 +94,13 @@ total_steps = 10
 
 
 
-# primal_chkpnt = '../logs_cons_more_data/logs_cons_more_data_new/updated/t_10/checkpoint_0/checkpoint'
-# primal_state = checkpoints.restore_checkpoint(primal_chkpnt, target=None)
-# primal_state = flax.core.FrozenDict(primal_state)
+primal_chkpnt = '../logs_cons_more_data/logs_cons_more_data_new/updated/t_10/checkpoint_0/checkpoint'
+primal_state = checkpoints.restore_checkpoint(primal_chkpnt, target=None)
+primal_state = flax.core.FrozenDict(primal_state)
 primal_model = primal(primal_config)
 dual_model = dual(dual_config)
 
-primal_chkpnts = [f'../logs_cons/t_{i}/checkpoint_0/checkpoint' for i in range(1, 11)]
+primal_chkpnts = [f'../logs_cons_more_data/logs_cons_more_data_new/updated/t_{i}/checkpoint_0/checkpoint' for i in range(1, 11)]
 
 
 dt = 0.1
@@ -324,69 +314,25 @@ def solve_minimax_primal(params, x, p, t):
 # dual value funcs
 @partial(jit, static_argnums=(3, ))
 def dual_value_final(params, x, p_hat, t):
-    r1 = 5
-    r2 = 1
     v1 = params[:2]
-    v2 = params[2:4]
-    v3 = params[4:6]
-    l1 = params[6]
-    l2 = params[7]
-    p_hat_1 = params[8:10]
-    p_hat_2 = params[10:12]
-    p_hat_3 = params[12:14]
-    u1 = params[28:30]
-    u2 = params[30:32]
-    u3 = params[32:34]
+    u1 = params[4:6]
 
-    l3 = 1 - l1 - l2 + EPSILON
-    # use conditional
-    # l3_true = lambda: 1 - l1 - l2
-    # l3_false = lambda: 0.
+    p_hat_1 = p_hat
 
-    # l3 = jax.lax.cond(l1 + l2 < 1, l3_true, l3_false)
-    # p_hat_3 = (p_hat - l1 * p_hat_1 - l2 * p_hat_2)/l3
-    cons_phat = jnp.sqrt(jnp.sum((p_hat - l1 * p_hat_1 - l2 * p_hat_2 - l3 * p_hat_3)**2) + 1e-12)
-
-    # l3 = 1 - l1 - l2 + EPSILON
-    # p_hat_3 = (p_hat - l1 * p_hat_1 - l2 * p_hat_2)/l3
-    # p_hat_3 = p_hat_3.clip(-p_hat_bound, p_hat_bound) should not clip
-
-
-    x_next_1 = x_next(x, u1, v1)
-    x_next_2 = x_next(x, u2, v2)
-    x_next_3 = x_next(x, u3, v3)
+    x_next_1 = x_next(x, u1, v1, dt=dt)
 
 
     ins_cost_1 = dt * utils_jax.running_cost(u1, v1).reshape(-1, )
-    ins_cost_2 = dt * utils_jax.running_cost(u2, v2).reshape(-1, )
-    ins_cost_3 = dt * utils_jax.running_cost(u3, v3).reshape(-1, )
 
     p_hat_1 = p_hat_1 - ins_cost_1
-    p_hat_2 = p_hat_2 - ins_cost_2
-    p_hat_3 = p_hat_3 - ins_cost_3
+
 
     val_1 = final_cost_function_dual(x_next_1, p_hat_1)
-    val_2 = final_cost_function_dual(x_next_2, p_hat_2)
-    val_3 = final_cost_function_dual(x_next_3, p_hat_3)
 
-    # regularization
-    p2_params = params[:14]
-    z = params[14:28]
-    p1_params = params[28:34]
-    v = params[34:]
-
-    # reg1 = (r1 / 2) * jnp.sum((p2_params - z) ** 2)
-    # reg2 = (r2 / 2) * jnp.sum((p1_params - v) ** 2)
-
-    lam_penalty = 8 * relu(-l3)  # if lambda_3 is negative penalty is high
-    p_hat_penalty = 8 * relu(cons_phat)
 
     final_pen_1 = final_feasibility(x_next_1)
-    final_pen_2 = final_feasibility(x_next_2)
-    final_pen_3 = final_feasibility(x_next_3)
 
-    objective = (l1 * val_1 + l2 * val_2 + l3 * val_3 + p_hat_penalty + lam_penalty
-                 - 1000 * relu(-final_pen_1) - 1000 * relu(-final_pen_2) - 1000 * relu(-final_pen_3)) # + reg1 - reg2
+    objective = (val_1 - 1000 * relu(-final_pen_1))
 
     return objective.reshape(())
 
@@ -396,47 +342,19 @@ def dual_value_inter(params, x, p_hat, t):
     chkpt_idx = int(10 * t) - 1  # current model
     weights_dir = chkpnts[chkpt_idx - 1]  # next model, we need next model in optimization
     state_dict = checkpoints.restore_checkpoint(ckpt_dir=weights_dir, target=None)
-    r1 = 5
-    r2 = 1
+
     v1 = params[:2]
-    v2 = params[2:4]
-    v3 = params[4:6]
-    l1 = params[6]
-    l2 = params[7]
-    p_hat_1 = params[8:10]
-    p_hat_2 = params[10:12]
-    p_hat_3 = params[12:14]
-    u1 = params[28:30]
-    u2 = params[30:32]
-    u3 = params[32:34]
+    u1 = params[4:6]
 
-    l3 = 1 - l1 - l2 + EPSILON
-    # use conditional
-    # l3_true = lambda: 1 - l1 - l2
-    # l3_false = lambda: 0.
+    p_hat_1 = p_hat
 
-    # l3 = jax.lax.cond(l1 + l2 < 1, l3_true, l3_false)
-    # p_hat_3 = (p_hat - l1 * p_hat_1 - l2 * p_hat_2)/l3
-    cons_phat = jnp.sqrt(jnp.sum((p_hat - l1 * p_hat_1 - l2 * p_hat_2 - l3 * p_hat_3)**2) + 1e-12)
-
-    # l3 = 1 - l1 - l2 + EPSILON
-    # p_hat_3 = (p_hat - l1 * p_hat_1 - l2 * p_hat_2)/l3
-    # p_hat_3 = p_hat_3.clip(-p_hat_bound, p_hat_bound) # we should not clip
-
-
-    x_next_1 = x_next(x, u1, v1)
-    x_next_2 = x_next(x, u2, v2)
-    x_next_3 = x_next(x, u3, v3)
-
+    x_next_1 = x_next(x, u1, v1, dt=dt)
 
     ins_cost_1 = dt * utils_jax.running_cost(u1, v1).reshape(-1, )
-    ins_cost_2 = dt * utils_jax.running_cost(u2, v2).reshape(-1, )
-    ins_cost_3 = dt * utils_jax.running_cost(u3, v3).reshape(-1, )
 
 
     input_1 = jnp.concat((x_next_1, p_hat_1 - ins_cost_1))
-    input_2 = jnp.concat((x_next_2, p_hat_2 - ins_cost_2))
-    input_3 = jnp.concat((x_next_3, p_hat_3 - ins_cost_3))
+
 
     v_bound_next_1_x = compute_bounds(1 - t + dt, 6)
     v_bound_next_1_y = compute_bounds(1 - t + dt, 12)
@@ -451,28 +369,11 @@ def dual_value_inter(params, x, p_hat, t):
         return dual_model.apply(state_dict, rescaled_input)
 
     val_1 = apply_to_model(input_1)
-    val_2 = apply_to_model(input_2)
-    val_3 = apply_to_model(input_3)
-
-    # regularization
-    p2_params = params[:14]
-    z = params[14:28]
-    p1_params = params[28:34]
-    v = params[34:]
-
-    reg1 = (r1 / 2) * jnp.sum((p2_params - z) ** 2)
-    reg2 = (r2 / 2) * jnp.sum((p1_params - v) ** 2)
-
-
-    lam_penalty = 8 * relu(-l3)  # if lambda_3 is negative penalty is high
-    p_hat_penalty = 8 * relu(cons_phat)
 
     from_brt_1, _ = brt_value(t - dt, x_next_1)
-    from_brt_2, _ = brt_value(t - dt, x_next_2)
-    from_brt_3, _ = brt_value(t - dt, x_next_3)
 
-    objective = (l1 * val_1 + l2 * val_2 + l3 * val_3 + p_hat_penalty + lam_penalty - 1000 * relu(-from_brt_1) -
-                 1000 * relu(-from_brt_2) - 1000 * relu(-from_brt_3))#+ reg1 - reg2
+
+    objective = (val_1 - 1000 * relu(-from_brt_1))
 
     return objective.reshape(())
 
@@ -483,14 +384,14 @@ def gradient_step_dual(params, x, p, loss_fn, t):
     beta = 0.4
     mu = 0.5
     grad = jax.grad(loss_fn, argnums=0)(params, x, p, t)
-    p2_params = params[:14]
-    z = params[14:28]
-    p1_params = params[28:34]
-    v = params[34:]
-    new_p2_params = (p2_params - c * grad[:14]).clip(min_bounds_dual[:14], max_bounds_dual[:14])
+    p2_params = params[:2]
+    z = params[2:4]
+    p1_params = params[4:6]
+    v = params[6:]
+    new_p2_params = (p2_params - c * grad[:2]).clip(min_bounds_dual[:2], max_bounds_dual[:2])
     new_params = jnp.concatenate((new_p2_params, z, p1_params, v))
     grad = jax.grad(loss_fn, argnums=0)(new_params, x, p, t)
-    new_p1_params = (p1_params + alpha * grad[28:34]).clip(min_bounds_dual[14:], max_bounds_dual[14:])
+    new_p1_params = (p1_params + alpha * grad[4:6]).clip(min_bounds_dual[2:], max_bounds_dual[2:])
     new_z = z + beta * (new_p2_params - z)
     new_v = v + mu * (new_p1_params - v)
     new_params = jnp.concatenate((new_p2_params, new_z, new_p1_params, new_v))
@@ -507,14 +408,14 @@ def solve_minimax_dual(params, x, p_hat, t):
         val_fn = dual_value_inter
 
     curr_params = params
-    iters = 200000
+    iters = 120000
     iter = 0
     with tqdm(total=iters) as pbar:
         while iter <= iters:
             new_params, norm1, norm2 = gradient_step_dual(curr_params, x, p_hat, val_fn, t)
             curr_params = new_params
-            p2_params = curr_params[:14]
-            p1_params = curr_params[28:34]
+            p2_params = curr_params[:2]
+            p1_params = curr_params[4:6]
             # if not iter % 1000:
             #     print(norm1)
             pbar.update(1)
@@ -535,18 +436,26 @@ if __name__ == '__main__':
     # y1s = [-0.5, -0.5]
     # y2s = [-0.4, -0.4]
 
+    # for type-1:
+    # x1s = [0.5, -0.5]
+    # x2s = [0.4, -0.4]
+    # y1s = [-0.5, -0.5]
+    # y2s = [-0.3, -0.4]
+
     # for type-2:
-    x1s = [-0.5]
-    x2s = [-0.4]
-    y1s = [0]
-    y2s = [0.2]
+    x1s = [-0.5, 0.5]
+    x2s = [-0.4, 0.3]
+    y1s = [0, 0]
+    y2s = [-0.2, -0.2]
+
+
 
     # x1s = [-0.5]
 
-    for run in range(len(x1s)):  # number of simulations to run
+    for run in range(0, 2):  # number of simulations to run
         type_map = {0: 'Goal 2', 1: 'Goal 1'}
-        p1_type = 1
-
+        p1_type = 0
+        flag = None
         # print(f'P1 type is: {type_map[p1_type]}')
 
         p_t = 0.5
@@ -568,27 +477,27 @@ if __name__ == '__main__':
 
 
         # input to primal value network
-        # input_to_primal = jnp.hstack((curr_state, jnp.array([[p_t]]))).squeeze()
+        input_to_primal = jnp.hstack((curr_state, jnp.array([[p_t]]))).squeeze()
 
-        # val_fn = lambda x: primal_model.apply(primal_state, x)
-        #
-        # primal_value = val_fn(input_to_primal)
-        # primal_value_grad = jax.jacfwd(val_fn)(input_to_primal)
-        #
-        # dvdp = primal_value_grad[:, -1]
+        val_fn = lambda x: primal_model.apply(primal_state, x)
 
-        # p_hat_2 = primal_value - p_t * dvdp
-        # p_hat_1 = p_hat_2 + dvdp
-        p_hat_1 = -0.02466
-        p_hat_2 = -0.02466
+        primal_value = val_fn(input_to_primal)
+        primal_value_grad = jax.jacfwd(val_fn)(input_to_primal)
+
+        dvdp = primal_value_grad[:, -1]
+
+        p_hat_2 = primal_value - p_t * dvdp
+        p_hat_1 = p_hat_2 + dvdp
+        # p_hat_1 = -0.02466
+        # p_hat_2 = -0.02466
 
         p_hat_t = jnp.array([p_hat_1, p_hat_2]).T.squeeze()
 
         # states.append(jnp.hstack((curr_state, p_hat_t.reshape(1, -1))))
         states.append(jnp.hstack((curr_state, jnp.array([[p_t]]))))
 
-        # print("belief is:", p_t)
-        # print("p_hat is:", p_hat_t)
+        print("belief is:", p_t)
+        print("p_hat is:", p_hat_t)
 
         # a_max = 12  # maximum control bound for both players
         key = jax.random.PRNGKey(0)
@@ -636,12 +545,10 @@ if __name__ == '__main__':
 
 
             # dual optimization second
-            params_d = jax.random.uniform(key5, (1, 6), minval=-1, maxval=1)
-            params_d_lam = jnp.array([[EPSILON, 1 - EPSILON]])
-            params_d_phat = jax.random.uniform(key6, (1, 6), minval=-p_hat_bound, maxval=p_hat_bound)
-            params_u = jax.random.uniform(key6, (1, 6), minval=-1, maxval=1)
+            params_d = jax.random.uniform(key5, (1, 2), minval=-1, maxval=1)
+            params_u = jax.random.uniform(key6, (1, 2), minval=-1, maxval=1)
 
-            params = jnp.concatenate((params_d, params_d_lam, params_d_phat, params_u), axis=1)
+            params = jnp.concatenate((params_d, params_u), axis=1)
             params = np.repeat(params, repeats=1, axis=0)
 
             params = jnp.repeat(params, repeats=2, axis=0).reshape(1, -1)  # repeat for dsgda
@@ -649,63 +556,48 @@ if __name__ == '__main__':
             params = solve_minimax_dual(params.reshape(-1, ), curr_state.reshape(-1, ), p_hat_t, t)
             # print(f'Parameters are: {params}')
             params = params.reshape(-1, )
-            p2_params = params[:14]
-            p1_params = params[14:]
+            p2_params = params[:2]
+            p1_params = params[2:]
 
             v1 = p2_params[:2]
-            v2 = p2_params[2:4]
-            v3 = p2_params[4:6]
 
-            u1_dual = p1_params[0:2]
-            u2_dual = p1_params[2:4]
-            u3_dual = p1_params[4:6]
+            u1_dual = p1_params[:2]
 
-            # compute instantaneous losses
-            l1 = p2_params[6]
-            l2 = p2_params[7]
-            if l1 + l2 < 1:
-                l3 = 1 - l1 - l2
-            else:
-                l3 = 0.
 
             ins_cost_1 = dt * utils_jax.running_cost(u1_dual, v1).reshape(-1, )
-            ins_cost_2 = dt * utils_jax.running_cost(u2_dual, v2).reshape(-1, )
-            ins_cost_3 = dt * utils_jax.running_cost(u3_dual, v3).reshape(-1, )
 
-            p_hat_1 = p2_params[8:10] - ins_cost_1
-            p_hat_2 = p2_params[10:12] - ins_cost_2
-            p_hat_3 = p2_params[12:14] - ins_cost_3
 
-            # p_hat_3 = (p_hat_t - l1 * p_hat_1 - l2 * p_hat_2) / l3
-            # p_hat_3 = p_hat_3.clip(-p_hat_bound, p_hat_bound)
+            p_hat_1 = p_hat_t - ins_cost_1
+
+
 
             print(f'At t = {1 - t:.2f}, P1 with type:\"{type_map[p1_type]}\" has the following choices: \n')
             print(f'P1 could take action {u1} with probability {u_1_prob:.2f} and update belief to {pos_1:.2f}')
             print(f'P1 could take action {u2} with probability {u_2_prob:.2f} and update belief to {pos_2:.2f}')
 
-            print(f'At t = {1 - t:.2f}, P2 has the following choices: \n')
-            print(f'P2 could take action {v1} with probability {l1:.2f} and update p_hat to {p_hat_1}')
-            print(f'P2 could take action {v2} with probability {l2:.2f} and update p_hat to {p_hat_2}')
-            print(f'P2 could take action {v3} with probability {l3:.2f} and update p_hat to {p_hat_3}')
+            # print(f'At t = {1 - t:.2f}, P2 has the following choices: \n')
+            # print(f'P2 could take action {v1} with probability {l1:.2f} and update p_hat to {p_hat_1}')
 
 
-            dist = jnp.array([l1, l2, l3])
+            # dist = jnp.array([l1, l2, l3])
+            #
+            # dist = dist/(dist.sum())  # tms
+            #
+            # a_idx = [0, 1, 2]
+            #
+            # action_idx = random.choices(a_idx, dist)[0]
 
-            dist = dist/(dist.sum())  # tms
-
-            a_idx = [0, 1, 2]
-
-            action_idx = random.choices(a_idx, dist)[0]
-
-            if action_idx == 0:
-                p2_action = v1
-                p_hat_t = p_hat_1
-            elif action_idx == 1:
-                p2_action = v2
-                p_hat_t = p_hat_2
-            else:
-                p2_action = v3
-                p_hat_t = p_hat_3
+            # if action_idx == 0:
+            #     p2_action = v1
+            #     p_hat_t = p_hat_1
+            # elif action_idx == 1:
+            #     p2_action = v2
+            #     p_hat_t = p_hat_2
+            # else:
+            #     p2_action = v3
+            #     p_hat_t = p_hat_3
+            p2_action = v1
+            p_hat_t = p_hat_1
 
             print(f'P1 chooses: {p1_action}, and P2 chooses: {p2_action}. The belief is now {p_t:.2f}.'
                   f' The p_hat is now {p_hat_t}')
@@ -723,7 +615,8 @@ if __name__ == '__main__':
             _, infeasible = brt_value(t, curr_state)
 
             if infeasible and t!= 0:
-                # flag = jnp.ones(10)
+                # raise ValueError('New state is infeasible! Game Ends!')
+                flag = jnp.ones(10)
                 print('Infeasibe at: ', t)
             else:
                 print(f'current state is {curr_state}')
@@ -778,6 +671,7 @@ if __name__ == '__main__':
 
         import pandas as pd
         df1 = pd.DataFrame(data={'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2, 'p': p_t})
-        df1.to_csv(f'cons_primal_dual_type_{p1_type}_{run}.csv')
+        df1.to_csv(f'cons_primal_dual_{p1_type}_{run}.csv')
+
 
     plt.show()
